bb1f29d98d98e9666431a3c3f5ef492db28f1372
mhabib5073 <m.habib5073@gmail.com>
2023-12-03 14:53:19 +0530

    1stapp

---
+++ testApp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+import json
+
+def emp_data_view(request):
+    emp_data = {'eno':101,'ename':'Rohit','esal':1000,'eaddr':'Hyderabad'}
+    resp='<h1>Employee No:{}<br>Employee Name:{}<br>Employee Salary:{}<br>Employee Address:{}</h1>'.format(emp_data['eno'],emp_data['ename'],emp_data['esal'],emp_data['eaddr'])
+    return HttpResponse(resp)
+def emp_data_json_view(request):
+    emp_data = {'eno':101,'ename':'Rohit','esal':1000,'eaddr':'Hyderabad'}
+    # jsn_data = json.dump(emp_data)
+    # return HttpResponse(jsn_data,content_type='application/json')
+    return JsonResponse(emp_data)
+
+from django.views.generic import View
+class JsonCBV(View):
+    def get(self,request,*args,**kwargs):
+       emp_data = {'eno':101,'ename':'Rohit','esal':1000,'eaddr':'Hyderabad'}
+       
+       return JsonResponse(emp_data)
+    
+
+
